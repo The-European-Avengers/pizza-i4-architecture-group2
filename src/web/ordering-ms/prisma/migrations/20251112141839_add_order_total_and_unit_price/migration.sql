31200abca63e8b6f15cfc9b2fbc956fee838d61d
Jeremy-Ramirez <jerarami@espol.edu.ec>
2025-11-15 22:41:03 +0100

    ordering ms fix

--- src/web/ordering-ms/prisma/migrations/20251112141839_add_order_total_and_unit_price/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "total_price" DECIMAL(65,30) NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "Order_Pizza" ADD COLUMN     "unit_price" DECIMAL(65,30) NOT NULL DEFAULT 0;
