ceb4e2a84668bd2e9570c9592aeb29d631b1273a
Miguel Aréjula Aísa <arejula10@gmail.com>
2025-12-03 19:55:59 +0100

    update data after running the experiment reducing the max stock value

--- src/web/scripts/script.sh
+++ src/web/scripts/script.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
+# Create 10 orders of each size (1, 10, 50, 100 pizzas) in an interleaved pattern
 for i in {1..10}; do
     npx tsx newOrders.ts 1 true
-done
-for i in {1..10}; do
     npx tsx newOrders.ts 10 false
-done
-
-for i in {1..10}; do
     npx tsx newOrders.ts 50 true
-done
-
-for i in {1..10}; do
     npx tsx newOrders.ts 100 false
 done
